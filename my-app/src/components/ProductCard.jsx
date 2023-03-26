b6220f08fec1b8e5449eafcd176ea3b8ea3aebf3
balja0803 <balja0803@gmail.com>
2023-03-26 22:45:04 +0800

    filter

--- my-app/src/components/ProductCard.jsx
+++ my-app/src/components/ProductCard.jsx
@@ -1,21 +1,21 @@
 import Card from "react-bootstrap/Card";
 import BasketLogo from "../subcomponent/BasketLogo";
 
-export default function ProductCard({ list }) {
+export default function ProductCard({ product }) {
   return (
     <Card border="primary" style={{ width: "250px", height: " 300px" }}>
-      <Card.Title>{list.brand.name} </Card.Title>
+      <Card.Title>{product.brand.name} </Card.Title>
       <Card.Img
         variant="top"
         style={{ width: "150px", height: "150px", borderRadius: "10px" }}
-        src={list.image[0]}
+        src={product.image[0]}
       />
       <Card.Body>
-        <Card.Title>{list.name}</Card.Title>
-        <Card.Text>{list.price}$</Card.Text>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>{product.price}$</Card.Text>
 
         <Card.Text style={{ fontWeight: "bold" }}>
-          {list.category.name}
+          {product.category.name}
         </Card.Text>
         {/* <BasketLogo /> */}
       </Card.Body>
